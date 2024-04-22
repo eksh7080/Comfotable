e7679904fffce889e1485b297d5904c65fea2e81
Lee Dano <eksh7080@gmail.com>
2024-04-22 20:16:26 +0900

    프로젝트 생성

---
+++ oauthlogin/public/styles/theme.tsx
@@ -0,0 +1,22 @@
+import { Theme } from '@emotion/react';
+
+const theme: Theme = {
+    colors: {
+        black: '#000',
+        white: '#fff',
+        coral: '#FF7F50',
+        blue: '#0000ff',
+        red: '#ff0000',
+        halfBlack: 'rgba(0,0,0,0.5)',
+        themeColor: '#6495ed',
+    },
+    fontSize: {
+        small: '1rem',
+        normal: '1.2rem',
+        base: '1.4rem',
+        title: '1.6rem',
+        big: '2.4rem',
+    },
+};
+
+export default theme;
---
+++ oauthlogin/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styled from '@emotion/styled/macro';
+
+const Container = styled.section`
+  max-width: 100%;
+`;
+
+const KaKao = styled.button``;
+
+export default function Home() {
+  return <Container></Container>;
+}
